de7b38b17d36922c22d7839f3eee91b854ee2c08
alexis39tt <alessandro.fonzi@gmail.com>
2022-04-24 14:08:21 +0200

    handled movie flags

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -6,8 +6,13 @@
           :src="`https://image.tmdb.org/t/p/w185/${elm.poster_path}`"
           :alt="elm.title"
         />
-        {{ elm.title }} - {{ elm.original_title }} -
-        {{ elm.original_language }} - {{ elm.vote_average }}
+        <div class="lang">
+          <img v-show="elm.original_language == 'en'" src="https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg" :alt="elm.original_language">
+          <img v-show="elm.original_language == 'it'" src="https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg" :alt="elm.original_language">
+          <img v-show="elm.original_language == 'xx' || ''" src="https://upload.wikimedia.org/wikipedia/commons/1/11/Globe_Flag.svg" :alt="elm.original_language">
+        </div>
+        
+        {{ elm.title }} - {{ elm.original_title }} - {{ elm.vote_average }}
       </div>
     </div>
   </main>
